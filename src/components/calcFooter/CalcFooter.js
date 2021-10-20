522a2b79b08a76f2ef35357c715982fde2e4c3b9
RafaelSS <Rafaelss427@gmail.com>
2021-10-20 13:25:51 -0600

    Botones renderizados correctamente

--- src/components/calcFooter/CalcFooter.js
+++ src/components/calcFooter/CalcFooter.js
@@ -1,4 +1,5 @@
-import CalcBoton from '../calcBoton/CalcBoton'
+// import CalcBoton from '../calcBoton/CalcBoton'
+import { Fragment } from 'react'
 import { tiposBotones } from '../../helpers/tiposBotones'
 
 import './calcFooter.css'
@@ -7,13 +8,17 @@ const CalcFooter = () => {
     return(
         <div className="calcFooter-contenedor">
             {
-                // tiposBotones.map(filaBtn => {
-
-
-                //     return (
-                //         <button></button>
-                //     )
-                // })
+                tiposBotones.map((fila, i) => {
+                    return (
+                        <Fragment key={i}>
+                            {
+                                fila.map(btn => (
+                                    <button key={btn.name}>{ btn.name }</button>
+                                ))
+                            }
+                        </Fragment>
+                    )
+                })
             }
         </div>
     )
